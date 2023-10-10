af92a8b6ea6f3f5c28cd5268e19c6902d938acfb
Gokkulnath <gokkulnath@gmail.com>
2023-10-10 19:57:38 +0530

    update requirements and dataset download part

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,2 +1,2 @@
 pip install gdown
-gdown --fuzzy https://drive.google.com/drive/folders/1T5wkBC43CLYBPJKh4wToyq4W5Md6UtwC?usp=sharing -O dataset --folder
+gdown --fuzzy https://drive.google.com/drive/folders/1T5wkBC43CLYBPJKh4wToyq4W5Md6UtwC?usp=sharing -O Mosquito_Dataset --folder
